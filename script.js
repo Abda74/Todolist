77973be9f4c27497f7a1b41ef824e5efe46e0c6d
Abdala NIGNAN <nignanamed0@gmail.com>
2023-12-20 15:21:00 +0000

    Deuxieme commit

--- script.js
+++ script.js
@@ -1,93 +1,81 @@
-//recuperation des ids
 
-const champ = document.getElementById("champ");
-const ol = document.getElementById("ol");
-const form = document.getElementById("form");
-const noTache = document.getElementById("notache");
-
-
- form.addEventListener("submit", function (even){
-     const li =document.createElement("li")
-
-     const texte = document.createElement("span");
-     texte.classList.add("texte");
-     texte.textContent = champ.value;
-
-     const spanli = document.createElement("span");
-     spanli.classList.add("delete")
-     spanli.innerHTML = "<i class=\"fa-solid fa-trash-can\"></i>" ;
-     const spanUrge = document.createElement("span");
-     spanUrge.classList.add("urge");
-     spanUrge.innerHTML = "<i class=\"fa-regular fa-star\"></i>" ;
-
-
-     li.appendChild(texte);
-     li.appendChild(spanUrge)
-     li.appendChild(spanli)
-
-     ol.appendChild(li);
-     champ.value = "";
-     noTache.style.display = "none" ;
-     localStorage.setItem("list",ol.innerHTML);
-     even.preventDefault();
-
-     spanli.addEventListener("click", function (){
-         li.remove();
-         noTache.style.display = (ol.innerHTML === "")? "block":"none";
-         localStorage.setItem("list",ol.innerHTML);
-     })
-
-     spanUrge.addEventListener("click", function () {
-         spanUrge.classList.toggle("gold");
-     })
-
-
-})
-
-
-ol.innerHTML = localStorage.getItem("list");
-
-const spanDel = document.querySelectorAll(".delete");
-for (let span of spanDel){
-    span.addEventListener("click", function (){
-        span.parentElement.remove();
-    })
-}
-noTache.style.display = (ol.innerHTML === "")? "block":"none";
-
+//Prendre en compte la taille du clavier
 const metas = document.getElementsByTagName("meta");
 metas[1].content = `width=device-width, ${innerHeight}, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0`
 
-window.addEventListener('load', () => {
-    // Is service worker available?
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js').then(() => {
-            console.log('Service worker registered!');
-        }).catch((error) => {
-            console.warn('Error registering service worker:');
-            console.warn(error);
-        });
-    }
-});
+//recuperaton de la balise form
+const form = document.getElementById("form");
+const ol = document.getElementById("ol");
+const noTache = document.getElementById("notache");
+ form.addEventListener("submit", function (even){
 
-const installbtn = document.getElementById("installBtn");
+     const champ = document.getElementById("champ");
 
-window.addEventListener("", function (event) {
+
+     //Creation d'une liste
+     const li =document.createElement("li")
+     const texte = document.createElement("span");
+     const spanli = document.createElement("span");
+     const spanUrge = document.createElement("span");
+
+     texte.classList.add("texte");
+     spanli.classList.add("delete");
+     spanUrge.classList.add("urge");
+
+     texte.textContent = champ.value;
+
+     spanli.innerHTML = "<i class=\"fa-solid fa-trash-can\"></i>"; //icone corbeile;
+     spanUrge.innerHTML = "<i class=\"fa-regular fa-star\"></i>" ; //icone etoile
+
+     //Ajout des spans creer dans la liste
+     li.appendChild(texte);
+     li.appendChild(spanUrge)
+     li.appendChild(spanli)
+     ol.appendChild(li);
+
+     //Vider le champ input de type texte lorsqu'une tâche a été ajouter
+     champ.value = "";
+     noTache.style.display = (ol.innerHTML === "")? "block":"none";
+     localStorage.setItem("list" , ol.innerHTML);
+     even.preventDefault();
+
+     //Suppression d'une liste
+     spanli.addEventListener("click", function (){
+         del(li);
+         localStorage.setItem("list" , ol.innerHTML);
+         noTache.style.display = (ol.innerHTML === "")? "block":"none";
+     });
+
+     //Ajouter une liste comme favorie
+     spanUrge.addEventListener("click", function () {
+         spanUrge.classList.toggle("gold");
+     });
+
 
 
 })
 
-window.beforinstallprompt = (event)=> {
-    event.preventDefault();
-
-    installbtn.classList.add("slide");
-
-    installbtn.addEventListener("click", function () {
-        installbtn.classList.remove("slide");
-        setTimeout( ()=> installbtn.style.display="none", 800);
-        event.prompt;
-    });
+function del(element) {
+    element.remove();
 }
 
+    ol.innerHTML = localStorage.getItem("list");
+
+const spanDel = document.querySelectorAll(".delete");
+spanDel.forEach(span =>
+    span.addEventListener("click" , ()=>{
+        del(span.parentElement);
+        localStorage.setItem("list" , ol.innerHTML);
+    }));
+noTache.style.display = (ol.innerHTML === "")? "block":"none";
+
+const installbtn = document.getElementById("installBtn");
+    setTimeout(()=>{
+        installbtn.classList.add("slide");
+    }, 3000);
+
+setTimeout(()=>{
+    installbtn.classList.remove("slide");
+}, 6000);
 
 
